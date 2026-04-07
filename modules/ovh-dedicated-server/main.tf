79c4bbe692a3759c4ea88f0940b8fff45942b152
matustoth <55988328+karafra@users.noreply.github.com>
2026-04-07 19:55:04 +0200

    Remove import

--- modules/ovh-dedicated-server/main.tf
+++ modules/ovh-dedicated-server/main.tf
@@ -3,7 +3,6 @@ data "ovh_me" "account" {}
 resource "ovh_dedicated_server" "server" {
   for_each                  = var.dedicated_servers
   ovh_subsidiary            = data.ovh_me.account.ovh_subsidiary
-  service_name              = each.value.service_name
   display_name              = try(each.value.display_name, each.value.service_name)
   boot_id                   = try(each.value.boot_id, null)
   monitoring                = try(each.value.monitoring, true)
